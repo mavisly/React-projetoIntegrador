fedb96ead0056d18b6e7f220e28d1694d25aab6c
lucas-alexandrino <lucas.alexcontato@gmail.com>
2023-04-19 13:24:54 -0300

    Comitando Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -34,27 +33,3 @@ function App() {
 }
 
 export default App
-=======
-import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import "./App.css";
-import Login from "./paginas/login/Login";
-import Teste from "./paginas/teste/teste";
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />;
-        <Route path="/home" element={<Teste />} />;
-        <Route path="/cadastro" element={<Teste />} />;
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-export default App;
->>>>>>> main
