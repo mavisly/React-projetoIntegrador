f51e979c2653e46ce6d98b743698017a7cae4850
Mavisly <mavi.04.02.03@gmail.com>
2023-04-19 10:10:15 -0300

    Teste Branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 
-import { useState } from "react";
 import {Route, Routes, BrowserRouter} from "react-router-dom";
 import "./App.css";
 import Login from "./pages/login/Login";
 import Home from "./pages/home/Home";
 import Cadastro from "./pages/cadastro/Cadastro";
-
+import Navbar from "./components/static/menu/Navbar";
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <BrowserRouter>
+    <Navbar />
       <Routes>
         <Route path="/" element={<Login />} />;
         <Route path="/home" element={<Home />} />;
