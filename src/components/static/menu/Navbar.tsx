fe4d5b4568302f15cc25002be7df5a659b23f289
Mavisly <mavi.04.02.03@gmail.com>
2023-04-26 11:20:00 -0300

    Início do CRUD

--- src/components/static/menu/Navbar.tsx
+++ src/components/static/menu/Navbar.tsx
@@ -3,11 +3,12 @@ import { AppBar, Toolbar, Typography, IconButton } from "@material-ui/core";
 import MenuIcon from '@material-ui/icons/Menu';
 import Button from "@material-ui/core/Button/Button";
 import { Box } from "@mui/material";
-
+import "./Navbar.css"
+import { Link, useNavigate } from "react-router-dom";
 
 function Navbar() {
     return (
-        <AppBar position="static">
+        <AppBar position="static" style={{ background: 'rgb(46,154,255' }}>
             <Toolbar variant="dense">
                 <IconButton edge="start" className="menuButton" aria-label="menu">
                     <MenuIcon />
@@ -19,54 +20,53 @@ function Navbar() {
                 </Box>
 
                 <Box display="flex" justifyContent="start">
-
-                    <Box mx={1} className='cursor'>
-                        <Typography variant="h6" color="inherit" >
-                            Home
-                        </Typography>
+                    <Link to="/home" className='text-decorator-none'>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit" >
+                                Home
+                            </Typography>
+                        </Box>
+                    </Link>
                     </Box>
-                </Box>
 
-
-                <Box display="flex" justifyContent="start">
-
-                    <Box mx={1} className='cursor'>
-                        <Typography variant="h6" color="inherit" >
-                            Postagens
-                        </Typography>
+                    <Box display="flex" justifyContent="start">
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit" >
+                                Postagens
+                            </Typography>
+                        </Box>
                     </Box>
-                </Box>
 
-                <Box display="flex" justifyContent="start">
+                    <Box display="flex" justifyContent="start">
 
-                    <Box mx={1} className='cursor'>
-                        <Typography variant="h6" color="inherit" >
-                            Temas
-                        </Typography>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit" >
+                                Temas
+                            </Typography>
+                        </Box>
                     </Box>
-                </Box>
 
-                <Box display="flex" justifyContent="start">
+                    <Box display="flex" justifyContent="start">
 
-                    <Box mx={1} className='cursor'>
-                        <Typography variant="h6" color="inherit" >
-                            Cadastrar Tema
-                        </Typography>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit" >
+                                Cadastrar Tema
+                            </Typography>
+                        </Box>
                     </Box>
-                </Box>
 
-                <Box display="flex" justifyContent="start">
+                    <Box display="flex" justifyContent="start">
 
-                    <Box mx={1} className='cursor'>
-                        <Typography variant="h6" color="inherit" >
-                            Logout
-                        </Typography>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit" >
+                                Logout
+                            </Typography>
+                        </Box>
                     </Box>
-                </Box>
 
-                <Button color="inherit">Login</Button>
+                    <Button color="inherit">Login</Button>
             </Toolbar>
-        </AppBar>
+        </AppBar >
 
     );
 }
